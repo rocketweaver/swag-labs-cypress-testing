c6205360e27571ab9755448163e16d53c0fbbe22
rocketweaver <arbhyaditya2@gmail.com>
2024-12-05 16:11:14 +0700

    feat: add validatte checkout after adding item test case

--- cypress/e2e/checkout.js
+++ cypress/e2e/checkout.js
@@ -1,13 +1,46 @@
 import { onHomePage } from "../support/page_objects/homePage";
+import { onCartPage } from "../support/page_objects/cartPage";
+import { onCheckoutPage } from "../support/page_objects/checkoutPage";
 
 describe("Valid Checkout", () => {
-    before(() => {
-        cy.visit("/");
-    })
+  const username = [
+    "standard_user",
+    // "error_user",
+    // "visual_user",
+    // "performance_glitch_user",
+    // "problem_user",
+  ];
 
-    it("Validate checkout after adding item to cart", () => {
-        onHomePage.addToCart(3).then(() => {
-            cy.get(".shopping_cart_link").click();
-        })
+  const user = {
+    firstName: "John",
+    lastName: "Doe",
+    postalCode: "1234",
+  };
+
+  before(() => {
+    cy.visit("/");
+  });
+
+  it("Validate checkout after adding item to cart", () => {
+    cy.wrap(username).each((username) => {
+      cy.loginToApp(username, "secret_sauce");
+
+      onHomePage.addToCart(3).then(({ _itemsName, _itemsPrice }) => {
+        cy.get(".shopping_cart_link").click();
+
+        onCartPage.checkProductList(_itemsName, _itemsPrice);
+        cy.contains("Checkout").click();
+
+        onCheckoutPage.submitForm(user);
+        cy.contains("Continue").click();
+
+        onCheckoutPage.checkProductOverview(_itemsName, _itemsPrice);
+        cy.contains("Finish").click();
+
+        cy.contains("Thank you for your order!");
+      });
+
+      cy.then(Cypress.session.clearCurrentSessionData);
     });
-})
+  });
+});
--- cypress/support/page_objects/checkoutPage.js
+++ cypress/support/page_objects/checkoutPage.js
@@ -3,19 +3,61 @@ import * as TestFactories from "./helpers/helpers";
 class CheckoutPage {
   constructor() {
     this._selectors = {
-      container: ".cart_item",
+      cardContainer: ".cart_item",
       name: "[data-test='inventory-item-name']",
       desc: ".inventory_item_desc",
       price: ".inventory_item_price",
+      cartBadge: ".shopping_cart_badge",
     };
   }
 
-  checkProductList() {
+  checkProductList(itemsName, itemsPrice) {
     TestFactories.getProductsAttribute(this._selectors).then(
       ({ productList }) => {
         TestFactories.isDatumNotEqual(productList.names);
+        TestFactories.isStringsEqual(itemsName, productList.names);
+        TestFactories.isNumbersEqual(itemsPrice, productList.prices);
       }
     );
+  }
+
+  checkProductOverview(itemsName, itemsPrice) {
+    this.checkProductList(itemsName, itemsPrice);
+
+    cy.contains("Payment Information").then(() => {
+      cy.contains("SauceCard #31337").should("be.visible");
+    });
+
+    cy.contains("Shipping Information").then(() => {
+      cy.contains("Free Pony Express Delivery!");
+    });
+
+    cy.contains("Price Total").then(() => {
+      const itemsTotal = itemsPrice.reduce(
+        (partialSum, a) => partialSum + a,
+        0
+      );
+
+      const tax = (itemsTotal * 0.08).toFixed(2);
+
+      const totalPrice = itemsTotal + parseFloat(tax);
+
+      cy.contains(`Item total: $${itemsTotal}`);
+      cy.contains(`Tax: $${tax}`);
+      cy.contains(`Total: $${totalPrice}`);
+    });
+  }
+
+  submitForm(user) {
+    const _user = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      postalCode: user.postalCode,
+    };
+
+    cy.get("[name='firstName']").focus().type(_user.firstName);
+    cy.get("[name='lastName']").focus().type(_user.lastName);
+    cy.get("[name='postalCode']").focus().type(_user.postalCode);
   }
 }
 
